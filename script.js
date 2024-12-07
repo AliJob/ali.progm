5c2c3e4925df97385654d37c1c9d397bff41e28e
AliJob <146086431+AliJob@users.noreply.github.com>
2024-12-07 10:03:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     emailjs.init("YOUR_USER_ID"); // Replace with your actual User ID from EmailJS
 
     const questions = [
-        {
             question: "George is ................ than Nick.",
             options: ["tall", "taller", "tallest"],
             correctAnswer: "taller"
@@ -17,6 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
             question: "My friend ..................... lives in Australia is a nurse.",
             options: ["who", "which", "whose"],
             correctAnswer: "who"
+        },
+        {
+            question: "I like walking in the park ............. hot days.",
+            options: ["at", "on", "in"],
+            correctAnswer: "in"
+        },
+        {
+            question: "Centuries ago, people .......... animals for food.",
+            options: ["transport", "played", "hunted"],
+            correctAnswer: "hunted"
+        },
+        {
+            question: "If he ................... the lottery, he'll go on a round-the-world trip",
+            options: ["won", "wins", "will win"],
+            correctAnswer: "wins"
+        },
+        {
+            question: "Who is the president of USA?",
+            options: ["Alisher Jobirov", "Donald Trump", "Ali", "Jobirov"],
+            correctAnswer: "Ali"
         },
     ];
 
