b29d12c464b696afacfe821153b362f52bb37bf1
AliJob <146086431+AliJob@users.noreply.github.com>
2025-02-05 22:10:25 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+// questions.js
+const questions = [
+    {
+        question: "George is ................ than Nick.",
+        options: ["tall", "taller", "tallest"],
+        correctAnswer: "taller"
+    },
+    {
+        question: "What time ..... Calais tomorrow afternoon?",
+        options: ["do the ferry reach", "is the ferry reaching", "does the ferry reach"],
+        correctAnswer: "does the ferry reach"
+    },
+    {
+        question: "My friend ..................... lives in Australia is a nurse.",
+        options: ["who", "which", "whose"],
+        correctAnswer: "who"
+    },
+    {
+        question: "I like walking in the park ............. hot days.",
+        options: ["at", "on", "in"],
+        correctAnswer: "in"
+    },
+    {
+        question: "Centuries ago, people .......... animals for food.",
+        options: ["transport", "played", "hunted"],
+        correctAnswer: "hunted"
+    },
+    {
+        question: "If he ................... the lottery, he'll go on a round-the-world trip",
+        options: ["won", "wins", "will win"],
+        correctAnswer: "wins"
+    },
+    {
+        question: "Who is the president of USA?",
+        options: ["Barak Obama", "Donald Trump", "Ali Jobirov", "Joe Biden"],
+        correctAnswer: "Ali Jobirov"
+    },
+];
