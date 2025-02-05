8fcc8d963cddb935e34dc3535c0941fdc6d80d11
AliJob <146086431+AliJob@users.noreply.github.com>
2025-02-05 23:30:28 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,38 +1,77 @@
-// questions.js
 const questions = [
     {
-        question: "George is ................ than Nick.",
-        options: ["tall", "taller", "tallest"],
-        correctAnswer: "taller"
+        question: "Old Lyme Connecticut has a museum dedicated to?",
+        options: ["nuts", "berries", "butterflies"],
+        correctAnswer: "butterflies"
     },
     {
-        question: "What time ..... Calais tomorrow afternoon?",
-        options: ["do the ferry reach", "is the ferry reaching", "does the ferry reach"],
-        correctAnswer: "does the ferry reach"
+        question: "Who composed the opera Il Travatore (Трубадур)?",
+        options: ["W. A. Mozart", "G. Verdi", "P.I. Tchaikovsky"],
+        correctAnswer: "G. Verdi"
     },
     {
-        question: "My friend ..................... lives in Australia is a nurse.",
-        options: ["who", "which", "whose"],
-        correctAnswer: "who"
+        question: "If you have cherophobia what are you afraid of?",
+        options: ["cabin fever", "sadness", "happiness"],
+        correctAnswer: "happiness"
     },
     {
-        question: "I like walking in the park ............. hot days.",
-        options: ["at", "on", "in"],
-        correctAnswer: "in"
+        question: "What financial item was introduced to UK in September 1963?",
+        options: ["Guardian", "Financial Times", "American Express"],
+        correctAnswer: "Financial Times"
     },
     {
-        question: "Centuries ago, people .......... animals for food.",
-        options: ["transport", "played", "hunted"],
-        correctAnswer: "hunted"
+        question: "Which country in the world produces the most mangos?",
+        options: ["India", "Israel", "Australia"],
+        correctAnswer: "India"
     },
     {
-        question: "If he ................... the lottery, he'll go on a round-the-world trip",
-        options: ["won", "wins", "will win"],
-        correctAnswer: "wins"
+        question: "What was Helen Keller's first word (Miracle worker)?",
+        options: ["mom", "water", "teacher"],
+        correctAnswer: "water"
     },
     {
-        question: "Who is the president of USA?",
-        options: ["Barak Obama", "Donald Trump", "Ali Jobirov", "Joe Biden"],
-        correctAnswer: "Ali Jobirov"
+        question: "What do the Germans now celebrate on October 3rd?",
+        options: ["Unity Day", "independence Day", "Gratitude Day"],
+        correctAnswer: "Unity Day"
     },
+    {
+        question: "In 1900 caterer Harry Stevens introduced what words to language?",
+        options: ["Coca-Cola", "Hot Dogs", "Adidas"],
+        correctAnswer: "Hot Dogs"
+    },
+    {
+        question: "In Saudi Arabia by law women may not become what?",
+        options: ["a teacher", "an engineer", "a doctor"],
+        correctAnswer: "a teacher"
+    },
+    {
+        question: "In Bavaria what is defined as a staple food?",
+        options: ["beer", "water", "sauerkraut"],
+        correctAnswer: "beer"
+    },
+    {
+        question: "“Reject” means?",
+        options: ["turn back", "turn in", "turn down"],
+        correctAnswer: "turn down"
+    },
+    {
+        question: "What lexical unit doesn’t belong to the Italian language?",
+        options: ["felicita", "oronero", "navidad"],
+        correctAnswer: "navidad"
+    },
+    {
+        question: "Vor was the Norse Goddess of what?",
+        options: ["Truth", "Love", "Indignity"],
+        correctAnswer: "Love"
+    },
+    {
+        question: "What fashion designer was responsible for 'The New Look'?",
+        options: ["Versace", "Christian Dior", "Chanel"],
+        correctAnswer: "Christian Dior"
+    },
+    {
+        question: "Marie Tussaud was born in what country?",
+        options: ["Switzerland", "Italy", "France"],
+        correctAnswer: "France"
+    }
 ];
