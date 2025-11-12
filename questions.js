638de5907cb9edbacb3c246cb51d38ebc6f3b856
AliJob <146086431+AliJob@users.noreply.github.com>
2025-11-12 23:59:57 +0300

    Restore questions array with trivia questions

--- questions.js
+++ questions.js
@@ -1,153 +1,33 @@
 // questions.js
 const questions = [
-    {
-        question: "Old Lyme Connecticut has a museum dedicated to?",
-        options: ["nuts", "berries", "butterflies"],
-        correctAnswer: "butterflies"
-    },
-    {
-        question: "Who composed the opera Il Travatore (Трубадур)?",
-        options: ["W. A. Mozart", "G. Verdi", "P.I. Tchaikovsky"],
-        correctAnswer: "G. Verdi"
-    },
-    {
-        question: "If you have cherophobia what are you afraid of?",
-        options: ["cabin fever", "sadness", "happiness"],
-        correctAnswer: "happiness"
-    },
-    {
-        question: "What financial item was introduced to UK in September 1963?",
-        options: ["Guardian", "Financial Times", "American Express"],
-        correctAnswer: "Financial Times"
-    },
-    {
-        question: "Which country in the world produces the most mangos?",
-        options: ["India", "Israel", "Australia"],
-        correctAnswer: "India"
-    },
-    {
-        question: "What was Helen Keller's first word (Miracle worker)?",
-        options: ["mom", "water", "teacher"],
-        correctAnswer: "water"
-    },
-    {
-        question: "What do the Germans now celebrate on October 3rd?",
-        options: ["Unity Day", "independence Day", "Gratitude Day"],
-        correctAnswer: "Unity Day"
-    },
-    {
-        question: "In 1900 caterer Harry Stevens introduced what words to language?",
-        options: ["Coca-Cola", "Hot Dogs", "Adidas"],
-        correctAnswer: "Hot Dogs"
-    },
-    {
-        question: "In Saudi Arabia by law women may not become what?",
-        options: ["a teacher", "an engineer", "a doctor"],
-        correctAnswer: "a teacher"
-    },
-    {
-        question: "In Bavaria what is defined as a staple food?",
-        options: ["beer", "water", "sauerkraut"],
-        correctAnswer: "beer"
-    },
-    {
-        question: "Reject means?",
-        options: ["turn back", "turn in", "turn down"],
-        correctAnswer: "turn down"
-    },
-    {
-        question: "What lexical unit doesn't belong to the Italian language?",
-        options: ["felicita", "oronero", "navidad"],
-        correctAnswer: "navidad"
-    },
-    {
-        question: "Vor was the Norse Goddess of what?",
-        options: ["Truth", "Love", "Indignity"],
-        correctAnswer: "Love"
-    },
-    {
-        question: "What fashion designer was responsible for 'The New Look'?",
-        options: ["Versace", "Christian Dior", "Chanel"],
-        correctAnswer: "Christian Dior"
-    },
-    {
-        question: "Marie Tussaud was born in what country?",
-        options: ["Switzerland", "Italy", "France"],
-        correctAnswer: "France"
-    },
-    {
-        question: "Mageiricophobia is the fear of what?",
-        options: ["cooking", "dancing", "drawing"],
-        correctAnswer: "cooking"
-    },
-    {
-        question: "What is the world's warmest sea?",
-        options: ["The Adriatic Sea", "The Red Sea", "The Black Sea"],
-        correctAnswer: "The Red Sea"
-    },
-    {
-        question: "What can be Safety, Tableaux or Swag?",
-        options: ["curtains in theatre", "electricity", "bodyguards"],
-        correctAnswer: "curtains in theatre"
-    },
-    {
-        question: "What's Lenin Peak?",
-        options: ["a monument in Central Asia", "the name of street in Tajikistan", "a majestic mountain in the Pamir Mountains"],
-        correctAnswer: "a majestic mountain in the Pamir Mountains"
-    },
-    {
-        question: "Who discovered the laws of gravity, motion, invented calculus?",
-        options: ["Plato", "Isaac Newton", "Aristotle"],
-        correctAnswer: "Isaac Newton"
-    },
-    {
-        question: "The initials pc on a medicine means it should be taken when?",
-        options: ["after meals", "the past century", "post surgery state"],
-        correctAnswer: "after meals"
-    },
-    {
-        question: "Schubert always slept with what on?",
-        options: ["on his desk", "spectacles - in case he got idea", "on the floor"],
-        correctAnswer: "spectacles - in case he got idea"
-    },
-    {
-        question: "Which country had the first women MPs 19 in 1907?",
-        options: ["Finland", "The Russian Federation", "India"],
-        correctAnswer: "Finland"
-    },
-    {
-        question: "Which drink did Bach enjoy so much he wrote a cantata for it?",
-        options: ["whisky", "red wine", "coffee"],
-        correctAnswer: "coffee"
-    },
-    {
-        question: "Cleopatra sometimes wore a fake what?",
-        options: ["wig for official duties", "arm for official duties", "beard for official duties"],
-        correctAnswer: "beard for official duties"
-    },
-    {
-        question: "When was abolished serfdom in Russia?",
-        options: ["on March 3, 1861", "On March 7, 1861", "On March 13, 1861"],
-        correctAnswer: "on March 3, 1861"
-    },
-    {
-        question: "What colour is iridium (редкий химический элемент)?",
-        options: ["dark blue", "light yellow", "steel grey"],
-        correctAnswer: "steel grey"
-    },
-    {
-        question: "How many regions are in Italy?",
-        options: ["35", "20", "18"],
-        correctAnswer: "20"
-    },
-    {
-        question: "Name Alice's pet cat?",
-        options: ["Dunah", "Donah", "Dinah"],
-        correctAnswer: "Dinah"
-    },
-    {
-        question: "Who was Dobby in Harry Potter (book)?",
-        options: ["a house elf", "a babysitter", "a magic stone"],
-        correctAnswer: "a house elf"
-    }
+    { question: "Old Lyme Connecticut has a museum dedicated to?", options: ["nuts", "berries", "butterflies"], correctAnswer: "butterflies" },
+    { question: "Who composed the opera Il Travatore (Трубадур)?", options: ["W. A. Mozart", "G. Verdi", "P.I. Tchaikovsky"], correctAnswer: "G. Verdi" },
+    { question: "If you have cherophobia what are you afraid of?", options: ["cabin fever", "sadness", "happiness"], correctAnswer: "happiness" },
+    { question: "What financial item was introduced to UK in September 1963?", options: ["Guardian", "Financial Times", "American Express"], correctAnswer: "Financial Times" },
+    { question: "Which country in the world produces the most mangos?", options: ["India", "Israel", "Australia"], correctAnswer: "India" },
+    { question: "What was Helen Keller's first word (Miracle worker)?", options: ["mom", "water", "teacher"], correctAnswer: "water" },
+    { question: "What do the Germans now celebrate on October 3rd?", options: ["Unity Day", "independence Day", "Gratitude Day"], correctAnswer: "Unity Day" },
+    { question: "In 1900 caterer Harry Stevens introduced what words to language?", options: ["Coca-Cola", "Hot Dogs", "Adidas"], correctAnswer: "Hot Dogs" },
+    { question: "In Saudi Arabia by law women may not become what?", options: ["a teacher", "an engineer", "a doctor"], correctAnswer: "a teacher" },
+    { question: "In Bavaria what is defined as a staple food?", options: ["beer", "water", "sauerkraut"], correctAnswer: "beer" },
+    { question: "Reject means?", options: ["turn back", "turn in", "turn down"], correctAnswer: "turn down" },
+    { question: "What lexical unit doesn't belong to the Italian language?", options: ["felicita", "oronero", "navidad"], correctAnswer: "navidad" },
+    { question: "Vor was the Norse Goddess of what?", options: ["Truth", "Love", "Indignity"], correctAnswer: "Love" },
+    { question: "What fashion designer was responsible for 'The New Look'?", options: ["Versace", "Christian Dior", "Chanel"], correctAnswer: "Christian Dior" },
+    { question: "Marie Tussaud was born in what country?", options: ["Switzerland", "Italy", "France"], correctAnswer: "France" },
+    { question: "Mageiricophobia is the fear of what?", options: ["cooking", "dancing", "drawing"], correctAnswer: "cooking" },
+    { question: "What is the world's warmest sea?", options: ["The Adriatic Sea", "The Red Sea", "The Black Sea"], correctAnswer: "The Red Sea" },
+    { question: "What can be Safety, Tableaux or Swag?", options: ["curtains in theatre", "electricity", "bodyguards"], correctAnswer: "curtains in theatre" },
+    { question: "What's Lenin Peak?", options: ["a monument in Central Asia", "the name of street in Tajikistan", "a majestic mountain in the Pamir Mountains"], correctAnswer: "a majestic mountain in the Pamir Mountains" },
+    { question: "Who discovered the laws of gravity, motion, invented calculus?", options: ["Plato", "Isaac Newton", "Aristotle"], correctAnswer: "Isaac Newton" },
+    { question: "The initials pc on a medicine means it should be taken when?", options: ["after meals", "the past century", "post surgery state"], correctAnswer: "after meals" },
+    { question: "Schubert always slept with what on?", options: ["on his desk", "spectacles - in case he got idea", "on the floor"], correctAnswer: "spectacles - in case he got idea" },
+    { question: "Which country had the first women MPs 19 in 1907?", options: ["Finland", "The Russian Federation", "India"], correctAnswer: "Finland" },
+    { question: "Which drink did Bach enjoy so much he wrote a cantata for it?", options: ["whisky", "red wine", "coffee"], correctAnswer: "coffee" },
+    { question: "Cleopatra sometimes wore a fake what?", options: ["wig for official duties", "arm for official duties", "beard for official duties"], correctAnswer: "beard for official duties" },
+    { question: "When was abolished serfdom in Russia?", options: ["on March 3, 1861", "On March 7, 1861", "On March 13, 1861"], correctAnswer: "on March 3, 1861" },
+    { question: "What colour is iridium (редкий химический элемент)?", options: ["dark blue", "light yellow", "steel grey"], correctAnswer: "steel grey" },
+    { question: "How many regions are in Italy?", options: ["35", "20", "18"], correctAnswer: "20" },
+    { question: "Name Alice's pet cat?", options: ["Dunah", "Donah", "Dinah"], correctAnswer: "Dinah" },
+    { question: "Who was Dobby in Harry Potter (book)?", options: ["a house elf", "a babysitter", "a magic stone"], correctAnswer: "a house elf" }
 ];
